2c87f7ae13437235f1d352d877e3ec84beb05308
omegalulcoder <ldc9966@nyu.edu>
2023-12-12 12:37:12 -0500

    added button

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,9 +1,11 @@
 int ledPin = 13;
 int ledPin2 = 14;
+int buttonPin = 15;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
